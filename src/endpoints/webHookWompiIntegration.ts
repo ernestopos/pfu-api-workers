43e18dd67d67570f857afd614ec108316991a9f6
ernestopos <ernestopos@hotmail.com>
2025-08-11 23:46:27 -0500

    integration
    integration

--- src/endpoints/webHookWompiIntegration.ts
+++ src/endpoints/webHookWompiIntegration.ts
@@ -57,6 +57,10 @@ export class webHookWompiIntegration extends OpenAPIRoute {
       return new Response("Status not supported, right now", { status: 200 });
     }
 
+    await c.env.DB.prepare(
+      "INSERT INTO PRUEBA(DATOS) VALUES(?)")
+      .bind(JSON.stringify(dataRequest)).all();
+
     if (dataRequest.data?.transaction?.shipping_address === null) {
       await generateAndDonationSend(c.env, {
         amount: Number(
