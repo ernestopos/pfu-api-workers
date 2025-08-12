13f60900fb4177af2fc2c04e7e8daf49f544f5db
ernestopos <ernestopos@hotmail.com>
2025-08-12 04:49:55 -0500

    Prueba 12082025
    Prueba 12082025

--- src/endpoints/webHookWompiIntegration.ts
+++ src/endpoints/webHookWompiIntegration.ts
@@ -86,10 +86,12 @@ export class webHookWompiIntegration extends OpenAPIRoute {
         },
       };
     } else {
-
+      let datosEnviados = " REFERENCE : " + dataRequest.data.transaction.reference +
+                          " ID_TRANSATION : " + dataRequest.data.transaction.id;
+                          
       await c.env.DB.prepare(
       "INSERT INTO PRUEBA(DATOS) VALUES(?)")
-      .bind("LLEGÓ AQUI 1.2").all();
+      .bind(datosEnviados).all();
 
       actualizarEstadoFactura(c.env, dataRequest);
       respondeData = {
